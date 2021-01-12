958e3722fa1fe373921e5274f1f0c3f3ec1c81cd
Sofia Lemons <sofia.lemons@earlham.edu>
2021-01-12 15:03:55 -0500

    Changed duplicate tracking in beam search to indicate whether a node was selected for the beam or not, and only prune based on nodes previously selected for the beam.

--- search/beam.hpp
+++ search/beam.hpp
@@ -1,143 +1,147 @@
-// Copyright © 2013 the Search Authors under the MIT license. See AUTHORS for the list of authors.                                                             
+// Copyright © 2020 the Search Authors under the MIT license. See AUTHORS for the list of authors.                                                             
 #pragma once                                                                    
 #include "../search/search.hpp"                                                 
 #include "../utils/pool.hpp"
                                                                                 
 template <class D> struct BeamSearch : public SearchAlgorithm<D> {
 
-  typedef typename D::State State;
-  typedef typename D::PackedState PackedState;                                
-  typedef typename D::Cost Cost;                                              
-  typedef typename D::Oper Oper;
+	typedef typename D::State State;
+	typedef typename D::PackedState PackedState;                                
+	typedef typename D::Cost Cost;                                              
+	typedef typename D::Oper Oper;
 
-  struct Node {
-    int openind;
-    Node *parent;
-    PackedState state;
-    Oper op, pop;
-    Cost f, g;
+	struct Node {
+		int openind;
+		bool selected;
+		Node *parent;
+		PackedState state;
+		Oper op, pop;
+		Cost f, g;
 
-    Node() : openind(-1) {
-    }
+		Node() : openind(-1) {
+		}
 
-    static ClosedEntry<Node, D> &closedentry(Node *n) {
-      return n->closedent;
-    }
+		static ClosedEntry<Node, D> &closedentry(Node *n) {
+			return n->closedent;
+		}
 
-    static PackedState &key(Node *n) {
-      return n->state;
-    }
+		static PackedState &key(Node *n) {
+			return n->state;
+		}
 
-    /* Set index of node on open list. */
-    static void setind(Node *n, int i) {
-      n->openind = i;
-    }
+		/* Set index of node on open list. */
+		static void setind(Node *n, int i) {
+			n->openind = i;
+		}
 
-    /* Get index of node on open list. */
-    static int getind(const Node *n) {
-      return n->openind;
-    }
+		/* Get index of node on open list. */
+		static int getind(const Node *n) {
+			return n->openind;
+		}
 
-    /* Indicates whether Node a has better value than Node b. */
-    static bool pred(Node *a, Node *b) {
-      if (a->f == b->f)
-        return a->g > b->g;
-      return a->f < b->f;
-    }
+		/* Indicates whether Node a has better value than Node b. */
+		static bool pred(Node *a, Node *b) {
+			if (a->f == b->f)
+				return a->g > b->g;
+			return a->f < b->f;
+		}
 
-    /* Priority of node. */
-    static Cost prio(Node *n) {
-      return n->f;
-    }
+		/* Priority of node. */
+		static Cost prio(Node *n) {
+			return n->f;
+		}
 
-    /* Priority for tie breaking. */
-    static Cost tieprio(Node *n) {
-      return n->g;
-    }
+		/* Priority for tie breaking. */
+		static Cost tieprio(Node *n) {
+			return n->g;
+		}
 
     private:
 		ClosedEntry<Node, D> closedent;
     
-  };
+	};
 
-  BeamSearch(int argc, const char *argv[]) :
-    SearchAlgorithm<D>(argc, argv), closed(30000001) {
-    dropdups = false;
-    for (int i = 0; i < argc; i++) {
-      if (i < argc - 1 && strcmp(argv[i], "-width") == 0)
-        width = atoi(argv[++i]);
-      if (strcmp(argv[i], "-dropdups") == 0)
-        dropdups = true;
-    }
+	BeamSearch(int argc, const char *argv[]) :
+		SearchAlgorithm<D>(argc, argv), closed(30000001) {
+		dropdups = false;
+		for (int i = 0; i < argc; i++) {
+			if (i < argc - 1 && strcmp(argv[i], "-width") == 0)
+				width = atoi(argv[++i]);
+			if (strcmp(argv[i], "-dropdups") == 0)
+				dropdups = true;
+		}
 
-    if (width < 1)
-      fatal("Must specify a >0 beam width using -width");
+		if (width < 1)
+			fatal("Must specify a >0 beam width using -width");
     
-    nodes = new Pool<Node>();
-  }
+		nodes = new Pool<Node>();
+	}
 
-  ~BeamSearch() {
-    delete nodes;
-  }
+	~BeamSearch() {
+		delete nodes;
+	}
 
-  void search(D &d, typename D::State &s0) {
-    this->start();
-    closed.init(d);
+	void search(D &d, typename D::State &s0) {
+		this->start();
+		closed.init(d);
 
-    Node *n0 = init(d, s0);
-    closed.add(n0);
-    open.push(n0);
+		Node *n0 = init(d, s0);
+		closed.add(n0);
+		open.push(n0);
 
-    int depth = 0;
+		int depth = 0;
 
 
-    bool solved = false;
+		bool solved = false;
     
-    while (!open.empty() && !solved && !SearchAlgorithm<D>::limit()) {
-      depth++;
+		while (!open.empty() && !solved && !SearchAlgorithm<D>::limit()) {
+			depth++;
       
-      Node **beam = new Node*[width];
-      int c;
-      for(c = 0; c < width && !open.empty(); c++) {
-        Node *n = open.pop();
+			Node **beam = new Node*[width];
+			int c;
+			for(c = 0; c < width && !open.empty(); c++) {
+				Node *n = open.pop();
 
-        beam[c] = n;
-      }
+				beam[c] = n;
+				n->selected = true;
+			}
 
-      open.clear();
+			while(!open.empty())
+			  open.pop();
+			//open.clear();
       
-      for(int i = 0; i < c && !solved && !SearchAlgorithm<D>::limit(); i++) {
-        Node *n = beam[i];
-        State buf, &state = d.unpack(buf, n->state);
+			for(int i = 0; i < c && !solved && !SearchAlgorithm<D>::limit(); i++) {
+				Node *n = beam[i];
+				State buf, &state = d.unpack(buf, n->state);
 
-        if (d.isgoal(state)) {
-          solpath<D, Node>(d, n, this->res);
-          solved = true;
-          break;
-        }
+				if (d.isgoal(state)) {
+					solpath<D, Node>(d, n, this->res);
+					solved = true;
+					break;
+				}
 
-        expand(d, n, state);
-      }
+				expand(d, n, state);
+			}
 
-      delete[] beam;
-    }
-    this->finish();
-  }
+			delete[] beam;
+		}
+		this->finish();
+	}
 
-  virtual void reset() {
-    SearchAlgorithm<D>::reset();
-    open.clear();
-    closed.clear();
-    delete nodes;
-    nodes = new Pool<Node>();
-  }
+	virtual void reset() {
+		SearchAlgorithm<D>::reset();
+		open.clear();
+		closed.clear();
+		delete nodes;
+		nodes = new Pool<Node>();
+	}
 
-  virtual void output(FILE *out) {
-    SearchAlgorithm<D>::output(out);
-    closed.prstats(stdout, "closed ");
-    dfpair(stdout, "open list type", "%s", open.kind());
-    dfpair(stdout, "node size", "%u", sizeof(Node));
-  }
+	virtual void output(FILE *out) {
+		SearchAlgorithm<D>::output(out);
+		closed.prstats(stdout, "closed ");
+		dfpair(stdout, "open list type", "%s", open.kind());
+		dfpair(stdout, "node size", "%u", sizeof(Node));
+	}
 
 
 private:
@@ -159,15 +163,16 @@ private:
 		assert (kid);
 		typename D::Edge e(d, state, op);
 		kid->g = parent->g + e.cost;
+		kid->selected = false;
 		d.pack(kid->state, e.state);
 
 		unsigned long hash = kid->state.hash(&d);
 		Node *dup = closed.find(kid->state, hash);
 		if (dup) {
 			this->res.dups++;
-			if (dropdups || kid->g >= dup->g) {
-              nodes->destruct(kid);
-              return;
+			if (dup->selected && (dropdups || kid->g >= dup->g)) {
+				nodes->destruct(kid);
+				return;
 			}
 			bool isopen = open.mem(dup);
 			if (isopen)
@@ -177,10 +182,12 @@ private:
 			dup->parent = parent;
 			dup->op = op;
 			dup->pop = e.revop;
+			dup->selected = false;
 			if (isopen) {
 				open.post_update(dup);
 			} else {
-				this->res.reopnd++;
+				if(dup->selected)
+				  this->res.reopnd++;
 				open.push(dup);
 			}
 			nodes->destruct(kid);
