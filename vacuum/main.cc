02d472873243b09288f28c5eb79e23b3a8fd004c
Sofia Lemons <cuddlegutz@gmail.com>
2023-01-10 11:20:00 -0500

    Added option for reading in cost in vacuum.

--- vacuum/main.cc
+++ vacuum/main.cc
@@ -7,6 +7,13 @@
 int main(int argc, const char *argv[]) {
 	dfheader(stdout);
 	Vacuum d(stdin);
+	const char *cost = "unit";
+	for (int i = 0; i < argc; i++) {
+		if(i < argc - 1 && strcmp(argv[i], "-cost") == 0)
+			cost = argv[++i];
+	}
+	
+	dfpair(stdout, "cost", "%s", cost);
 	search<Vacuum>(d, argc, argv);
 	dffooter(stdout);
 	return 0;
